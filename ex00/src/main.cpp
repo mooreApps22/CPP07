5b5a8fd478fc96ec262171766150185e6a108e65
mooreApps22 <124527392+mooreApps22@users.noreply.github.com>
2025-07-08 14:49:08 +0100

    Ex00 is working.

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -1,53 +1,50 @@
 #include <iostream>
-#include <Array.hpp>
+#include <string>
+#include "whatever.hpp"
 
-#define MAX_VAL 750
-int main(int, char**)
+
+template <typename Type>
+Type	min(Type a, Type b)
 {
-    Array<int> numbers(MAX_VAL);
-    int* mirror = new int[MAX_VAL];
-    srand(time(NULL));
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        const int value = rand();
-        numbers[i] = value;
-        mirror[i] = value;
-    }
-    //SCOPE
-    {
-        Array<int> tmp = numbers;
-        Array<int> test(tmp);
-    }
+	return (a < b) ? a : b;
+}
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        if (mirror[i] != numbers[i])
-        {
-            std::cerr << "didn't save the same value!!" << std::endl;
-            return 1;
-        }
-    }
-    try
-    {
-        numbers[-2] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    try
-    {
-        numbers[MAX_VAL] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
+template <typename Type>
+Type	max(Type a, Type b)
+{
+	Type result;
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        numbers[i] = rand();
-    }
-    delete [] mirror;//
-    return 0;
+	result = (a > b) ? a : b;
+	return result;
+}
+
+template <typename Type>
+void	swap(Type &a, Type &b)
+{
+	Type tmp;
+
+	tmp = a;
+	a = b;
+	b = tmp;
+}
+
+
+int main(void)
+{
+	int	a = 2;	
+	int	b = 3;	
+
+	::swap(a, b);
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b) = " << ::min(a,b) << std::endl;
+	std::cout << "max( a, b) = " << ::max(a,b) << std::endl;
+
+	std::string	c = "chaine1";	
+	std::string	d = "chaine2";	
+
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d) = " << ::min(c,d) << std::endl;
+	std::cout << "max( c, d) = " << ::max(c,d) << std::endl;
+	return 0;
 }

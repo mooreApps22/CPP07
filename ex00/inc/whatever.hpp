c5bf8b04e0ed496628fc86295284d60e0007bfc2
mooreApps22 <124527392+mooreApps22@users.noreply.github.com>
2025-07-08 14:59:46 +0100

    Had to fix ex00 to address not define the template in the header.

--- ex00/inc/whatever.hpp
+++ ex00/inc/whatever.hpp
@@ -1,10 +1,26 @@
 #pragma once
 
 template <typename Type>
-void	swap(Type &a, Type &b);
+void	swap(Type &a, Type &b)
+{
+	Type tmp;
+
+	tmp = a;
+	a = b;
+	b = tmp;
+}
 
 template <typename Type>
-Type	min(Type a, Type b);
+Type	min(Type a, Type b)
+{
+	return (a < b) ? a : b;
+}
 
 template <typename Type>
-Type	max(Type a, Type b);
+Type	max(Type a, Type b)
+{
+	Type result;
+
+	result = (a > b) ? a : b;
+	return result;
+}
--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -2,33 +2,6 @@
 #include <string>
 #include "whatever.hpp"
 
-
-template <typename Type>
-Type	min(Type a, Type b)
-{
-	return (a < b) ? a : b;
-}
-
-template <typename Type>
-Type	max(Type a, Type b)
-{
-	Type result;
-
-	result = (a > b) ? a : b;
-	return result;
-}
-
-template <typename Type>
-void	swap(Type &a, Type &b)
-{
-	Type tmp;
-
-	tmp = a;
-	a = b;
-	b = tmp;
-}
-
-
 int main(void)
 {
 	int	a = 2;	

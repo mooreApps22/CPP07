4f057d6a0a3b76501cd5b2ded28336fe7d13a011
Skyy Moore <mooreapps2022@gmail.com>
2025-07-08 22:20:26 +0100

    Working reasonably well.

--- ex02/inc/Array.hpp
+++ ex02/inc/Array.hpp
@@ -1,23 +1,46 @@
 #pragma once
 # include <iostream>
+# include <exception>
 
 template<typename T>
 class Array
 {
     public:
-		unsigned int size(void) const;
-        Array();
-        Array(unsigned int n);
-        Array(const Array& other);
-        Array& operator=(const Array& other);
-        T& operator[](unsigned int idx);
-        const T& operator[](unsigned int idx) const;
-        ~Array();
+		unsigned int size(void) const {
+			return _n;
+		}
+        Array() : _array(NULL), _n(0) {}
+        Array(unsigned int n) : _array(new T[n]()),  _n(n)  {}
+        Array(const Array& other) : _array(NULL), _n(0) {
+			*this = other;
+		}
+        Array& operator=(const Array& other) {
+			if (this != &other)	{
+				delete[] _array;
+				_n = other._n;
+				_array = new T[_n];
+				for (unsigned int i = 0; i < _n; ++ i)
+					_array[i] = other._array[i];
+			}
+			return (*this);
+		}
+        ~Array() { delete[] _array;}
+        T& operator[](unsigned int idx) {
+			if (idx >= _n)
+				throw indexOutOfBoundsException();
+			return (_array[idx]);
+		}
+        const T& operator[](unsigned int idx) const {
+			if (idx >= _n)
+				throw indexOutOfBoundsException();
+			return (_array[idx]);
+		}
 
-		class SomeException : public std::exception
-		{
+		class indexOutOfBoundsException : public std::exception {
 			public:
-				const char *what() const throw();
+				virtual const char *what() const throw() {
+					return ("That index is out of bounds!");
+				}
 		};
     private:
 		T*					_array;
--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -1,29 +1,23 @@
 #include <iostream>
 #include <string>
-#include "whatever.hpp"
-
-void	outputIntValue(int& x)
-{
-	std::cout << x << std::endl;
-}
-
-void	outputFloatValue(float& x)
-{
-	std::cout << x << std::endl;
-}
-
-void	outputStringValue(std::string& x)
-{
-	std::cout << x << std::endl;
-}
+#include "Array.hpp"
 
 int main(void)
 {
-	int nums1[] = {1, 2, 3, 4};
-	float nums2[] = {11.1, 22.2, 33.3, 44.4};
-	std::string strs1[] =  {"abc", "def", "ghi", "jkl"};
-	iter(nums1, 4, outputIntValue);
-	iter(nums2, 4, outputFloatValue);
-	iter(strs1, 4, outputStringValue);
+	Array<long>	a(5);
+
+	for (unsigned int i = 0; i < a.size(); ++i)
+		a[i] = i * 10;
+	for (unsigned int i = 0; i < a.size(); ++i)
+		std::cout << a[i] << std::endl;
+
+	try
+	{
+		std::cout << a[100] << std::endl;
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << "Caught exception: " << e.what() << std::endl;
+	}
 	return 0;
 }
